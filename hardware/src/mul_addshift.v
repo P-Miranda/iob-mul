ae3800846894028a9929622458dbee3244d0f937
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2021-01-27 01:42:26 +0000

    register result sign

--- hardware/src/mul_addshift.v
+++ hardware/src/mul_addshift.v
@@ -6,7 +6,6 @@ module mul_addshift
      )
    (
     input                     clk,
-
     input                     en,
     input                     sign,
     output reg                done,
@@ -16,8 +15,9 @@ module mul_addshift
     output reg [2*DATA_W-1:0] product
     );
 
+   reg                        sign_reg;
    reg [DATA_W-1:0]           op_a_reg; //multiplicand register
-   reg [$clog2(DATA_W):0]     pc;  //program counter register
+   reg [$clog2(DATA_W+1):0]   pc;  //program counter register
 
    //
    //PROGRAM
@@ -30,26 +30,28 @@ module mul_addshift
           
           0: begin //load multiplicand and product
              op_a_reg <= op_a;
-             if(sign)
-               product <= { op_b[0]? {op_a[DATA_W-1], op_a} : {DATA_W+1{1'b0}}, op_b[DATA_W-1:1] };
-             else
-               product <= { op_b[0]? op_a : 1'b0, op_b[DATA_W-1:1] };
-          end
-          
-          DATA_W-1: begin //arith shift then subtract last partial product
-             if(sign)
+             if(sign) begin
+                product <= { op_b[0]? {op_a[DATA_W-1], op_a} : {DATA_W+1{1'b0}}, op_b[DATA_W-1:1] };
+                sign_reg <= 1'b1;
+             end else begin
+                product <= { op_b[0]? op_a : 1'b0, op_b[DATA_W-1:1] };
+                sign_reg <= 1'b0;
+             end
+          end 
+          DATA_W-1: begin //last partial product
+             if(sign_reg) //subtract last partial product
                product <= { {product[2*DATA_W-1], product[2*DATA_W-1:DATA_W]} - (product[0]?  {op_a_reg[DATA_W-1], op_a_reg}: 1'b0), product[DATA_W-1:1]  };
-             else
+             else //add last partial product
                product <= { {1'b0, product[2*DATA_W-1:DATA_W]} + (product[0]?  {1'b0, op_a_reg}: 1'b0), product[DATA_W-1:1]  };
+             
+             done <= 1'b1;
+             
           end
           
-          DATA_W: begin //arith shift and finish   
-             pc <= pc;
-             done <= 1'b1;
-          end
+          DATA_W:  pc <= pc; //finish   
           
           default: begin //arith shift then add
-             if(sign)
+             if(sign_reg)
                product <= { {product[2*DATA_W-1], product[2*DATA_W-1:DATA_W]} + (product[0]?  {op_a_reg[DATA_W-1], op_a_reg}: 1'b0), product[DATA_W-1:1]  };
              else
                product <= { {1'b0, product[2*DATA_W-1:DATA_W]} + (product[0]?  {1'b0, op_a_reg}: 1'b0), product[DATA_W-1:1]  };
